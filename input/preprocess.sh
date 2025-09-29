8fae154fd1c4b0781626032b975a037631d980cf
Olukayode Olusegun <olukayodeoluseguno@gmail.com>
2025-09-29 16:59:08 +0200

    Removed some unwanted files

--- input/preprocess.sh
+++ input/preprocess.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 # Filtering out the row where Status column = "Failed" and selecting the needed columns.
+
 awk -F, 'BEGIN{OFS=","} $6 != "Failed" {print $1, $2, $3, $4, $5, $6}' raw_sales_data.csv > ~/data_pipeline/output/cleaned_sales_data.csv
-
-
-#echo "Data ingestion and transformation completed." > ~/data_pipeline/logs/preprocess.log
