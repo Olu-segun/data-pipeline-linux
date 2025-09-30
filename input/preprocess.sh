f5a2d3e8bdc7adda19cfa97d04ed094e33015c86
Olukayode Olusegun <olukayodeoluseguno@gmail.com>
2025-09-30 14:58:22 +0200

    Updated preprocess file

--- input/preprocess.sh
+++ input/preprocess.sh
@@ -2,4 +2,4 @@
 
 # Filtering out the row where Status column = "Failed" and selecting the needed columns.
 
-awk -F, 'BEGIN{OFS=","} $6 != "Failed" {print $1, $2, $3, $4, $5, $6}' raw_sales_data.csv > ~/data_pipeline/output/cleaned_sales_data.csv
+awk 'BEGIN {FS=",";  OFS=","} $6 != "Failed" {print $1, $2, $3, $4, $5, $6}' raw_sales_data.csv > ~/data_pipeline/output/cleaned_sales_data.csv
