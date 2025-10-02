8211605dce8adecaf1a7c399660ff1f151a0dd54
Olukayode Olusegun <olukayodeoluseguno@gmail.com>
2025-10-02 16:28:48 +0200

    Rearrange the file structure

--- input/extract_data.sh
+++ input/extract_data.sh
@@ -4,4 +4,4 @@
 
 curl -o raw_sales_data.csv "https://raw.githubusercontent.com/dataengineering-community/launchpad/refs/heads/main/Linux/sales_data.csv"
 
-echo "[$(date)] Data extraction completed successfully." | tee -a ~/data_pipeline/logs/preprocess.log 
+echo "[$(date)] Data extraction completed successfully." | tee -a ~/data_pipeline/logs/preprocess.log
--- input/preprocess.sh
+++ input/preprocess.sh
@@ -4,5 +4,4 @@
 
 awk 'BEGIN {FS=",";  OFS=","} $6 != "Failed" {print $1, $2, $3, $4, $5, $6}' raw_sales_data.csv > ~/data_pipeline/output/cleaned_sales_data.csv
 
-
-echo " [$(date)] Data processing completed sucessfully." | tee -a ~/data_pipeline/logs/preprocess.log
+echo "[$(date)] Data processing completed sucessfully." | tee -a ~/data_pipeline/logs/preprocess.log
