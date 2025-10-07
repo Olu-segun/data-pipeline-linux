6659a212605cca021cf5fe37d29884b615771148
Olukayode Olusegun <olukayodeoluseguno@gmail.com>
2025-10-07 12:07:35 +0200

    Corrected script for cron to save the cleaned sales data to output directory

--- input/preprocess.sh
+++ input/preprocess.sh
@@ -2,6 +2,6 @@
 
 # Filtering out the row where Status column = "Failed" and selecting the needed columns.
 
-awk 'BEGIN {FS=",";  OFS=","} $6 != "Failed" {print $1, $2, $3, $4, $5, $6}' raw_sales_data.csv > ~/data_pipeline/output/cleaned_sales_data.csv
+awk 'BEGIN {FS=",";  OFS=","} $6 != "Failed" {print $1, $2, $3, $4, $5, $6}' /home/olukayodeoluseguno-1/data_pipeline/input/raw_sales_data.csv > /home/olukayodeoluseguno-1/data_pipeline/output/cleaned_sales_data.csv
 
 echo "[$(date)] Data processing completed sucessfully." | tee -a ~/data_pipeline/logs/preprocess.log
