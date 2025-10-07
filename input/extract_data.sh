0c5dbfa4463e43105a850d5d76b82cba9a6eddbd
Olukayode Olusegun <olukayodeoluseguno@gmail.com>
2025-10-07 11:05:13 +0200

    Updated the data extraction file and others

--- input/extract_data.sh
+++ input/extract_data.sh
@@ -2,6 +2,7 @@
 
 # Extracting csv file from github repository
 
-curl -o raw_sales_data.csv "https://raw.githubusercontent.com/dataengineering-community/launchpad/refs/heads/main/Linux/sales_data.csv"
+curl -o /home/olukayodeoluseguno-1/data_pipeline/input/raw_sales_data.csv "https://raw.githubusercontent.com/dataengineering-community/launchpad/refs/heads/main/Linux/sales_data.csv"
 
+#Success Message
 echo "[$(date)] Data extraction completed successfully." | tee -a ~/data_pipeline/logs/preprocess.log
