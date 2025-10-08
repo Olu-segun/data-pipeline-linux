f9989c3a8499c08223bf2a2c637b553c49f751ef
Olukayode Olusegun <olukayodeoluseguno@gmail.com>
2025-10-08 22:40:28 +0200

    Updated scripts with variables

--- input/extract_data.sh
+++ input/extract_data.sh
@@ -2,7 +2,10 @@
 
 # Extracting csv file from github repository
 
-curl -o /home/olukayodeoluseguno-1/data_pipeline/input/raw_sales_data.csv "https://raw.githubusercontent.com/dataengineering-community/launchpad/refs/heads/main/Linux/sales_data.csv"
+data_path="https://raw.githubusercontent.com/dataengineering-community/launchpad/refs/heads/main/Linux/sales_data.csv"
+logs=~/data_pipeline/logs/preprocess.log
+
+curl -o /home/olukayodeoluseguno-1/data_pipeline/input/raw_sales_data.csv $data_path
 
 #Success Message
-echo "[$(date)] Data extraction completed successfully." | tee -a ~/data_pipeline/logs/preprocess.log
+echo "[$(date)] Data extraction completed successfully." | tee -a $logs
--- input/preprocess.sh
+++ input/preprocess.sh
@@ -2,6 +2,9 @@
 
 # Filtering out the row where Status column = "Failed" and selecting the needed columns.
 
-awk 'BEGIN {FS=",";  OFS=","} $6 != "Failed" {print $1, $2, $3, $4, $5, $6}' /home/olukayodeoluseguno-1/data_pipeline/input/raw_sales_data.csv > /home/olukayodeoluseguno-1/data_pipeline/output/cleaned_sales_data.csv
+output=~/data_pipeline/output/cleaned_sales_data.csv 
+logs=~/data_pipeline/logs/preprocess.log
 
-echo "[$(date)] Data processing completed sucessfully." | tee -a ~/data_pipeline/logs/preprocess.log
+awk 'BEGIN {FS=",";  OFS=","} $6 != "Failed" {print $1, $2, $3, $4, $5, $6}' /home/olukayodeoluseguno-1/data_pipeline/input/raw_sales_data.csv > $output
+
+echo "[$(date)] Data processing completed sucessfully." | tee -a $logs
